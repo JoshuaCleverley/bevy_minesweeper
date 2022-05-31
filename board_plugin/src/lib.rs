f344af4c8fd519e5150375973f41614ad205603a
JoshuaCleverley <joshuacleverley@protonmail.com>
2022-05-31 20:55:24 +0100

    Added a board entity with board options

--- board_plugin/src/lib.rs
+++ board_plugin/src/lib.rs
@@ -4,6 +4,9 @@ pub mod resources;
 use bevy::log;
 use bevy::prelude::*;
 use resources::tile_map::TileMap;
+use resources::BoardOptions;
+use resources::TileSize;
+use resources::BoardPosition;
 
 pub struct BoardPlugin;
 
@@ -15,10 +18,57 @@ impl Plugin for BoardPlugin {
 }
 
 impl BoardPlugin {
-    pub fn create_board() {
-        let mut tile_map = TileMap::empty(20, 20);
-        tile_map.set_bombs(40);
+    pub fn create_board(
+        mut commands: Commands,
+        board_options: Option<Res<BoardOptions>>,
+        window: Res<WindowDescriptor>,
+    ) {
+        let options = match board_options {
+            None => BoardOptions::default(),
+            Some(o) => o.clone(),
+        };
+        
+        let mut tile_map = TileMap::empty(options.map_size.0, options.map_size.1);
+        tile_map.set_bombs(options.bomb_count);
         #[cfg(feature = "debug")]
         log::info!("{}", tile_map.console_output());
+
+        let tile_size = match options.tile_size {
+            TileSize::Fixed(v) => v,
+            TileSize::Adaptive { min, max } => Self::adaptive_tile_size(
+                window, 
+                (min, max), 
+                (tile_map.width(), tile_map.height()),
+            )
+        };
+
+        let board_size = Vec2::new(
+            tile_map.width() as f32 * tile_size,
+            tile_map.height() as f32 * tile_size,
+        );
+        log::info!("Board size: {}", board_size);
+
+        let board_position = match options.position {
+            BoardPosition::Centered { offset } => {
+                Vec3::new(-(board_size.x / 2.0), -(board_size.y / 2.0), 0.0) + offset
+            }
+            BoardPosition::Custom(p) => p,
+        };
+
+        commands
+            .spawn()
+            .insert(Name::new("Board"))
+            .insert(Transform::from_translation(board_position))
+            .insert(GlobalTransform::default());
+    }
+
+    fn adaptive_tile_size(
+        window: Res<WindowDescriptor>,
+        (min, max): (f32, f32),
+        (width, height): (u16, u16),
+    ) -> f32 {
+        let max_width = window.width / width as f32;
+        let max_height = window.height / height as f32;
+        max_width.min(max_height).clamp(min, max)
     }
 }
